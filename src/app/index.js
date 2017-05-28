50e72c8cdd6228992a6519b80e35cd0ca96390ca
Manuel <ruizsantiago@gmail.com>
2017-05-28 11:39:45 +0200

    Sunday morning changes

--- src/app/index.js
+++ src/app/index.js
@@ -1,22 +1,54 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var getWeather = require('./getWeather');
+
+//Module requires
+var SearchCity = require('./search')
+
 
 //Create a component
 var CityComponent = React.createClass({
     getInitialState: function(){
-        return {
-            cities: ['Granada,ES']
-        }
+      return {
+          city: 'Granada',
+          country: 'ES'
+      }
     }, //getInitialState
+
     render: function(){
-      var cities = this.state.cities;
+      var city = this.state.city;
       return(
           <div id="city-list">
+            <header>
               <p>Weather Forecast</p>
-              <p>{this.state.cities[0]}</p>
+              <p>{this.state.city}, {this.state.country}</p>
+            </header>
+            <div>
+              <div id="search">
+                <SearchCity onChange={this.onChange} />
+              </div>
+              <div id="today"></div>
+            </div>
+            <div id="7days">
+
+            </div>
           </div>
       );
-    } //render
+    }, //render
+
+    //Custom functions
+    onChange: function(newCity){
+      var actualCity = this.state.city;
+      //console.log(newCity);
+      actualCity = newCity;
+      return(
+        getWeather.getWeather(actualCity)
+      );
+    } //onChange
 });
 
+
+
 ReactDOM.render(<CityComponent />, document.getElementById('city-wrapper'));
+
+module.exports = CityComponent;
