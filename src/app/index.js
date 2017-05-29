8a07d39c5f48c7fab0dbdd947ab98fa746beccdb
Manuel <ruizsantiago@gmail.com>
2017-05-29 07:04:57 +0200

    Successfully binding data

--- src/app/index.js
+++ src/app/index.js
@@ -1,31 +1,54 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var getWeather = require('./getWeather');
-
-//Module requires
-var SearchCity = require('./search')
-
+var Request = require('superagent');
+var jsonp = require('superagent-jsonp');
 
 //Create a component
 var CityComponent = React.createClass({
-    getInitialState: function(){
+    getInitialState: function() {
       return {
-          city: 'Granada',
-          country: 'ES'
-      }
+        name: '',
+        country: ''
+      };
+      this.SearchCity.search = this.SearchCity.search.bind(this);
     }, //getInitialState
 
+    componentDidMount: function(location) {
+      if (!location) { //Initial Location
+        this.setState({
+          name: 'Granada',
+          country: 'ES'
+        });
+      } else {
+        this.setState({
+          name: location.name,
+          country: location.country
+        });
+      } //Initial location
+
+      // const { types=['(cities)'] } = this.props;
+  		// this.autocomplete = new google.maps.places.Autocomplete(this.refs.location, {
+  		//   types,
+  		// });
+  		// this.autocomplete.addListener('place_changed', this.onSelected); //Autocomplete
+    }, //componentDidMount
+
+    handleSearch: function(location) {
+    }, //componentDidMount
+
     render: function(){
-      var city = this.state.city;
       return(
           <div id="city-list">
             <header>
               <p>Weather Forecast</p>
-              <p>{this.state.city}, {this.state.country}</p>
+              <p>{this.state.name}, {this.state.country}</p>
             </header>
             <div>
-              <div id="search">
-                <SearchCity onChange={this.onChange} />
+              <div id="handleChange">
+                <form id="search-city" onSubmit={this.handleSubmit}>
+                  <input type="text" name="searchCity" ref="newCity" required />
+                  <input type="submit" value="Search" />
+                </form>
               </div>
               <div id="today"></div>
             </div>
@@ -37,18 +60,38 @@ var CityComponent = React.createClass({
     }, //render
 
     //Custom functions
-    onChange: function(newCity){
-      var actualCity = this.state.city;
-      //console.log(newCity);
-      actualCity = newCity;
-      return(
-        getWeather.getWeather(actualCity)
-      );
-    } //onChange
+    handleSubmit: function(e) {
+      e.preventDefault();
+      this.search(this.refs.newCity.value);
+    }, //handleSubmit
+
+    search: function(newCity) {
+      const url = 'http://api.openweathermap.org/data/2.5/forecast/daily';
+      const unit = 'metric';
+      const days = 7;
+      var apiKey = '0c13455f76f055afc22e9a1cf9b67b7c';
+
+      var requestUrl = `${url}?q=${newCity}&cnt=${days}&APPID=${apiKey}`;
+      console.log(requestUrl);
+
+      Request.get(requestUrl).use(jsonp({
+        timeout: 3000,
+      })).then(function(res) {
+        if (res.body.cod === "200") {
+          var newCity = res.body.city.name;
+          var newCountry = res.body.city.country;
+
+          this.setState({
+            name: newCity,
+            country: newCountry
+          });
+        } else {
+          console.log(err);
+        }
+      }.bind(this));
+    } //search
 });
 
 
 
 ReactDOM.render(<CityComponent />, document.getElementById('city-wrapper'));
-
-module.exports = CityComponent;
