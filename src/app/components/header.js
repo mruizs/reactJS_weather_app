f0f31883e4664a90dc056b30cab3e68049d0aa51
Manuel <ruizsantiago@gmail.com>
2017-06-10 21:07:36 +0200

    ES6 completed

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -5,7 +5,7 @@ export default class Header extends React.Component {
     return (
       <header>
         <h1>Weather Forecast</h1>
-        <h2>{this.props.name}, {this.props.country}</h2>
+        <h2>{this.props.city}, {this.props.country}</h2>
       </header>
     )
   }
---
+++ src/app/components/today.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default class Today extends React.Component {
+  render() {
+    return (
+      <div id="today">
+        <h2>{this.props.temp.dt}</h2>
+        <p>Now: {this.props.temp.day} {this.props.symbol}</p>
+        <p>Max: {this.props.temp.max} {this.props.symbol}</p>
+        <p>Min: {this.props.temp.min} {this.props.symbol}</p>
+        <p>Morning: {this.props.temp.morn} {this.props.symbol}</p>
+        <p>Evening: {this.props.temp.eve} {this.props.symbol}</p>
+        <p>Night: {this.props.temp.night} {this.props.symbol}</p>
+        <p>Avg. Pressure for this week: {this.props.avgPressure}hpa</p>
+      </div>
+    )
+  }
+}
